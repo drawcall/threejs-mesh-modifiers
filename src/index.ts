688265fbda1c9527a74a6fbd9e1df79de596e0ac
ajie <yajieliu@tencent.com>
2017-05-11 22:22:39 +0800

    update v2.1.0
    update v2.1.0

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,12 @@
-export * from './IModifier';
 export * from './ModifierStack';
 
-export * from './core/FaceProxy';
-export * from './core/MeshProxy';
-export * from './core/VertexProxy';
-export * from './core/Modifier';
-export * from './core/verlet/VerletConnection';
-export * from './core/verlet/VerletVertex';
-
-export * from './math/Matrix';
-export * from './math/Matrix4';
 export * from './math/Phase';
 export * from './math/Range';
-export * from './math/Value';
 export * from './math/Vector2';
 export * from './math/Vector3';
 export * from './math/XMath';
 
-export * from './util/Dictionary';
-export * from './util/EventEmitter';
-export * from './util/Log';
 export * from './util/ModConstant';
-
-export * from './plugins/IMesh';
-export * from './plugins/IVector3';
-export * from './plugins/ThreeMesh';
-export * from './plugins/ThreeVertex';
 
 export * from './modifiers/Bend';
 export * from './modifiers/Bloat';
